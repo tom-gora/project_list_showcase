b05afe52b53ce1901ab5e94ba156d522aadc9ce3
tom-gora <goratomasz@outlook.com>
2023-08-04 01:28:10 +0100

    work on iframes

--- js/app.js
+++ js/app.js
@@ -1,9 +1,156 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const navigation = document.querySelector(".links-navbar");
-  const navigationToggle = navigation.querySelector(".toggle-navbar");
-  const navigationIcon = navigationToggle.querySelector("ion-icon");
+// grab a box where elements will be injected into
+const container = document.querySelector(".cards-container");
 
-  const typewriter = document.querySelector(".typewriter h3");
+let pages = [];
+
+// fetch my projects metadata json
+const fetchProjectList = async () => {
+  const response = await fetch(
+    "https://raw.githubusercontent.com/tom-gora/webdev_basics/master/fetch-me.json"
+  );
+  const projects = await response.json();
+  return projects;
+};
+
+// async respone
+fetchProjectList().then((response) => {
+  const projects = response.projects;
+
+  // sort to make sure my projects "timeline" is in order
+  const projectsSorted = projects.sort((a, b) => {
+    const dateA = new Date(a.date);
+    const dateB = new Date(b.date);
+    return dateA - dateB;
+  });
+
+  // loop through project objects to construct the page
+  projectsSorted.forEach((project) => {
+    //getting variables from the array coming from json
+    const skills = project.skills;
+    const categories = project.categories;
+    const dateObj = new Date(project.date);
+    const year = dateObj.getFullYear();
+    const monthNumber = dateObj.getMonth();
+    const day = dateObj.getDate();
+
+    // helper temp variables
+    let skillsHTML = "";
+    let categoriesHTML = "";
+    let counter = 2;
+
+    // map std date format to short month names strings
+    const monthNames = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    // set the name of the month
+    const month = monthNames[monthNumber];
+
+    // construct raw html for skills section
+    skills.forEach((skill) => {
+      skillsHTML += `<i class="devicon-${skill}-plain"></i>`;
+    });
+
+    // construct raw html for categories section
+    categories.forEach((category) => {
+      categoriesHTML += `<small>${category}</small>`;
+    });
+
+    // construct inner html for each card
+    const cardHtml = `<header class="skills">${skillsHTML}</header>
+      <div class="thumb" style="background-image: url('https://raw.githubusercontent.com/tom-gora${project.source}/thumbnail.png')";>
+        <button class="view-link">
+          <a href="#">
+            <small>View it!</small>
+            <ion-icon name="eye"></ion-icon>
+          </a>
+        </button>
+      </div>
+      <footer class="details" id="details-pane${counter}">
+        <button class="chevron-hint" aria-controls="details-pane${counter}">
+          <ion-icon name="chevron-up-circle"></ion-icon>
+        </button>
+        <div class="details-top">
+          <span class="categories">${categoriesHTML}</span>
+          <div class="date">
+            <div class="date-card">
+              <span>${day}</span>
+              <span>${month}</span>
+              <span>${year}</span>
+            </div>
+          </div>
+          <h2 class="title">${project.title}</h2>
+        </div>
+        <div class="details-bottom">
+          <p class="description">
+            ${project.description}
+          </p>
+          <button class="view-link">
+            <a href="#">
+              <small>View it!</small>
+              <ion-icon name="eye"></ion-icon>
+            </a>
+          </button>
+        </div>
+      </footer>`;
+
+    // prep temp element for card code injection
+    const tempElement = document.createElement("div");
+    //prep its attricutes as needed
+    tempElement.setAttribute("id", `card${counter}`);
+    tempElement.setAttribute("class", "card");
+    tempElement.setAttribute("data-size", "");
+    tempElement.setAttribute("data-expanded", "false");
+    tempElement.setAttribute("aria-expanded", "false");
+    // inject raw html into element
+    tempElement.innerHTML = cardHtml;
+
+    // append the completed card to the container
+    container.appendChild(tempElement);
+
+    // TODO: Get html content for iframes
+    let prepUrl = `https://raw.githubusercontent.com/tom-gora${project.source}/prepared.html`;
+
+    const getRawHTML = async (url) => {
+      const htmlDataResponse = await fetch(url);
+      const html = await htmlDataResponse.text();
+      pages.push(html);
+    };
+
+    getRawHTML(prepUrl);
+    counter++;
+  });
+
+  // console.log(pages);
+
+  //TODO: Continue
+  //
+  // const iframeBox = document.querySelector("#backdrop-render");
+  // const iframe = iframeBox.querySelector("#render");
+  // const iframeToggle = iframeBox.querySelector("#toggle-render");
+  // const viewLinks = document.querySelector(".view-link");
+  //
+  // viewLinks.forEach((viewLink) => {
+  //   viewLink.addEventListener("click", () => {
+  //     iframeBox.setAttribute("data-showing", "true");
+  //   });
+  // });
+  //
+  // iframeToggle.addEventListener("click", () => {
+  //   iframeBox.setAttribute("data-showing", "false");
+  // });
+  //
+  // rest of page functionality:
 
   const toggles = document.querySelectorAll(".chevron-hint");
   const cards = document.querySelectorAll(".card");
@@ -110,10 +257,12 @@ window.addEventListener("DOMContentLoaded", () => {
     });
   });
 
+  // if window resized past breakpoint in either direction it will be reloaded
   const resetCards = () => {
     console.log("reset");
     location.reload();
   };
+
   const observer = new MutationObserver((mutationsList) => {
     for (const mutation of mutationsList) {
       if (
@@ -133,6 +282,13 @@ window.addEventListener("DOMContentLoaded", () => {
     attributeOldValue: true, // Include the old value in the mutation object
   });
 
+  // rest of code non-dependant on async response
+  // navbar reposnsiveness
+  const navigation = document.querySelector(".links-navbar");
+  const navigationToggle = navigation.querySelector(".toggle-navbar");
+  const navigationIcon = navigationToggle.querySelector("ion-icon");
+
+  // mobile nav sliding in/out
   const handleMobileNav = () => {
     const menuState = navigation.getAttribute("data-expanded");
     if (menuState === "false") {
@@ -151,7 +307,29 @@ window.addEventListener("DOMContentLoaded", () => {
     typewriter.setAttribute("data-animate", "true");
   };
 
-  var typewriterObserver = new IntersectionObserver(
+  // handle dark theme switcher
+  const themeToggle = document.querySelector("#toggle-mode");
+  const themeToggleIcon = themeToggle.querySelector("ion-icon");
+  const themeToggleText = themeToggle.querySelector("span");
+
+  themeToggle.addEventListener("click", () => {
+    const body = document.querySelector("body");
+    const currentTheme = body.getAttribute("data-theme");
+
+    if (currentTheme === "dark") {
+      body.setAttribute("data-theme", "light");
+      themeToggleText.textContent = "Go Dark!";
+      themeToggleIcon.setAttribute("name", "moon");
+    } else {
+      body.setAttribute("data-theme", "dark");
+      themeToggleText.textContent = "Go Light!";
+      themeToggleIcon.setAttribute("name", "sunny");
+    }
+  });
+
+  // make sure the bottom animation only plays once in the viewport
+  const typewriter = document.querySelector(".typewriter h3");
+  let typewriterObserver = new IntersectionObserver(
     (entries) => {
       if (entries[0].isIntersecting === true) animateTypewriter();
     },
